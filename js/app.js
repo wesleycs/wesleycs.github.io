49bf9b92e15730607cd24b998c99372b67ed6d98
Wesley Csendom <wesleycs@buffalo.edu>
2016-05-11 20:46:33 -0400

    commiting changes

---
+++ js/app.js
@@ -0,0 +1,27 @@
+(function(){
+	var app = angular.module('store', [ ]);
+
+	app.controller('StoreController', function(){
+	this.products = gems;
+
+});
+
+var gems = [
+
+	{
+	name: "Dodecahedron", 
+	price: 2.95,
+	description: "Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.",
+	canPurchase: true,	
+	},
+
+	{
+	name: "Pentagonal Gem", 
+	price: 5.95,
+	description: " . . . ",
+	canPurchase: false,
+	}
+
+	];
+
+})();
